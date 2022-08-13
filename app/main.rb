32adf219742030473973037151aabea6b49aa732
Laurin Muth <l.muth@lyniat.games>
2022-08-13 15:21:11 +0200

    Add placeholder enemy

--- app/main.rb
+++ app/main.rb
@@ -15,8 +15,30 @@ def tick args
   @vector_y ||= 0
   @flipped ||= false
 
+  @bullets ||= []
+
+  #if args.state.tick_count == 0
+  #  @bullets << {x: 400, y: 400, dir_x: 1, dir_y: 1, speed: 0.1}
+  #end
+
   fish_mid_x = (Constants::FISH_W * Constants::FISH_SCALE) / 2
   fish_mid_y = (Constants::FISH_H * Constants::FISH_SCALE) / 2
+
+  if args.state.tick_count % 20 == 0
+    start_x = rand(1280)
+    start_y = rand(2) == 0 ? 720 + 190 : 0 -190
+
+    vector_bullet_x = (@x + fish_mid_x) - start_y
+    vector_bullet_y = (@y + fish_mid_y) - start_y
+
+    vector_bullet_v = Math.sqrt((vector_bullet_x ** 2) + (vector_bullet_y ** 2))
+
+    vector_bullet_x /= vector_bullet_v
+    vector_bullet_y /= vector_bullet_v
+
+    bullet = {x: start_x, y: start_y, dir_x: vector_bullet_x, dir_y: vector_bullet_y, speed: 2}
+    @bullets << bullet
+  end
 
   @vector_x = args.inputs.mouse.x - (@x + fish_mid_x)
   @vector_y = args.inputs.mouse.y - (@y + fish_mid_y)
@@ -67,6 +89,35 @@ def tick args
     h: Constants::FISH_H * Constants::FISH_SCALE,
     flip_horizontally: @flipped
   }
+
+  to_remove = []
+
+  @bullets.each do |bullet|
+    bullet[:x] += bullet[:dir_x] * bullet[:speed]
+    bullet[:y] += bullet[:dir_y] * bullet[:speed]
+
+    bx = bullet[:x]
+    by = bullet[:y]
+
+    if bx < -200 or bx > 1280 + 200 or by < -200 or by > 720 + 200
+      to_remove << bullet
+    end
+
+    args.outputs[:scene].sprites << {
+      path: "sprites/lyniat.png",
+      x: bx,
+      y: by,
+      w: 50,
+      h: 50
+    }
+
+    if Math.sqrt(((@x + fish_mid_x) - (bx + 25)) ** 2 + ((@y + fish_mid_y) - (by + 25)) ** 2) < 70
+      to_remove << bullet
+    end
+
+  end
+
+  @bullets -= to_remove
 
   args.outputs[:lighted_scene].sprites << { x: 0, y: 0, w: 1280, h: 720, path: :lights, blendmode_enum: 0 }
   args.outputs[:lighted_scene].sprites << { blendmode_enum: 2, x: 0, y: 0, w: 1280, h: 720, path: :scene }
