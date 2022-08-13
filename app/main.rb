59fe5e9f2191d2cb498a6835024ec58debe6c1ff
Laurin Muth <l.muth@lyniat.games>
2022-08-13 11:16:46 +0200

    Add fish animation

--- app/main.rb
+++ app/main.rb
@@ -1,3 +1,18 @@
+module Constants
+  FISH_W = 556
+  FISH_H = 515
+  FISH_SCALE = 0.25
+end
+
 def tick args
+  frames_fish = ["00", "01", "02", "03", "02", "01"]
   args.outputs.background_color = [117, 176, 185]
+  frame = (args.state.tick_count / 20).floor % 6
+  args.outputs.sprites << {
+    path: "sprites/fish_light_#{frames_fish[frame]}.png",
+    x: 0,
+    y: 0,
+    w: Constants::FISH_W * 0.25,
+    h: Constants::FISH_H * 0.25
+  }
 end
